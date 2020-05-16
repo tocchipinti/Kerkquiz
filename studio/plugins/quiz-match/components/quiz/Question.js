77057cc2ad0bf0abc9ec5cf446f03f47cc885308
Tim Occhipinti <t.occhipinti@outlook.com>
2020-05-16 13:51:27 +0200

    updates

--- studio/plugins/quiz-match/components/quiz/Question.js
+++ studio/plugins/quiz-match/components/quiz/Question.js
@@ -3,7 +3,6 @@ import client from 'part:@sanity/base/client'
 import imageUrlBuilder from '@sanity/image-url'
 import styles from '../styles/Question.css'
 import Icons from '../Icons'
-import AnswerCount from './AnswerCount'
 import Countdown from './Countdown'
 import {findCurrentQuestion} from '../../utils'
 import YouTube from 'react-youtube';
@@ -14,6 +13,7 @@ function urlFor(source) {
 }
 
 class Question extends React.Component {
+
   handleCloseQuestion = () => {
     this.props.onCloseQuestion()
   }
@@ -70,7 +70,7 @@ class Question extends React.Component {
               {questionImageUrl && <img className={styles.imageSrc} src={questionImageUrl} />}
             </div>
             <div className={styles.questionVideo}>
-              {youtubeId && <YouTube className={styles.youtubeHeader} videoId={youtubeId} opts={opts} onReady={this._onReady} />}
+              {youtubeId && <YouTube className={styles.youtubeHeader} videoId={youtubeId} opts={opts} onEnd={this._onEnd} onReady={this._onReady} />}
             </div>
             <h1 className={styles.questionTitle}>{group}</h1>
             <h1
@@ -93,7 +93,11 @@ class Question extends React.Component {
 
   _onReady(event) {
     event.target.setVolume(100);
-    event.target.playVideo();  
+    event.target.playVideo();
+  }
+  _onEnd(event) {
+    event.target.seekTo(1);
+    event.target.pauseVideo();
   }
 }
 
